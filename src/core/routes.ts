c4cc67cc3e5aad2171d2d2a54b2841fc5f75a6d3
felipe barreiros <felipewww@outlook.com.br>
2021-04-13 10:40:17 -0300

    more routes

--- src/core/routes.ts
+++ src/core/routes.ts
@@ -3,8 +3,30 @@ import {Express, NextFunction, Request, Response} from "express";
 export default class Routes {
     constructor(app: Express) {
 
+        app.get('/', (req: Request, res: Response, next: NextFunction) => {
+            return res.json({
+                name: 'index',
+                status: true
+            })
+        })
+
+        app.get('/health-check', (req: Request, res: Response, next: NextFunction) => {
+            return res.json({
+                name: 'health-check',
+                status: true
+            })
+        })
+
         app.get('/test', (req: Request, res: Response, next: NextFunction) => {
             return res.json({
+                name: 'test',
+                status: true
+            })
+        })
+
+        app.use((req: Request, res: Response, next: NextFunction) => {
+            return res.json({
+                name: '404',
                 status: true
             })
         })
